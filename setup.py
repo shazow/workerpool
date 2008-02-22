18b7cb44f1d4f2dc7239aadee4113be058161718
shazow <shazow@ac04cf61-a246-0410-96fa-1bc2aea7bcce>
2008-02-22 02:07:49 +0000

    Added license and readme
    git-svn-id: http://workerpool.googlecode.com/svn/trunk@4 ac04cf61-a246-0410-96fa-1bc2aea7bcce

--- setup.py
+++ setup.py
@@ -1,7 +1,31 @@
+#!/usr/bin/python
+
+# Copyright (c) 2008 Andrey Petorv (andrey.petrov@shazow.net)
+#
+# Permission is hereby granted, free of charge, to any person obtaining 
+a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish, dis-
+# tribute, sublicense, and/or sell copies of the Software, and to permit
+# persons to whom the Software is furnished to do so, subject to the fol-
+# lowing conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABIL-
+# ITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT
+# SHALL THE AUTHOR BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
+
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+version = '0.9'
 
 setup(name='workerpool',
       version=version,
@@ -11,9 +35,9 @@ Super elegant, super expandable, super fun.""",
       classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
       keywords='pooling, threading, jobs',
       author='Andrey Petrov',
-      author_email='apetrov@ideeinc.com',
-      url='',
-      license='',
+      author_email='andrey.petrov@shazow.net',
+      url='http://code.google.com/p/workerpool/',
+      license='MIT',
       packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
       include_package_data=True,
       zip_safe=False,
