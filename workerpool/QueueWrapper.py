f508fd105a2521afe65466b85c8ddf88f5f6bdd4
shazow <shazow@ac04cf61-a246-0410-96fa-1bc2aea7bcce>
2008-03-10 02:36:55 +0000

    Fixed a comment.
    git-svn-id: http://workerpool.googlecode.com/svn/trunk@27 ac04cf61-a246-0410-96fa-1bc2aea7bcce

--- workerpool/QueueWrapper.py
+++ workerpool/QueueWrapper.py
@@ -1,4 +1,4 @@
-# NewQueue.py - Implements Python 2.5 Queue functionality for Python 2.4
+# QueueWrapper.py - Implements Python 2.5 Queue functionality for Python 2.4
 # Copyright (c) 2008 Andrey Petrov
 #
 # This module is part of workerpool and is released under
